8b0dbe9003d2db91ad51ae7a4e5c5c8790dbf3ac
AIE\s162868 <edwardngo92@gmail.com>
2018-07-31 13:51:59 +1000

    updated NPC (bear, owl, croc, redpanda) scripts, added test scene

--- End Game/Assets/Scripts/NPC/NPC.cs
+++ End Game/Assets/Scripts/NPC/NPC.cs
@@ -8,105 +8,57 @@ using UnityEngine.UI;
 // Script written by: Edward Ngo
 //=================================================================================
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(NavMeshAgent))]
+
 public class NPC : MonoBehaviour {
 
-    private GameObject player;
-    private GameObject playerCam;
-    private NavMeshAgent NMA;
-    private Rigidbody rb;
-
     public bool isSearching;
-    private Image hello;
 
     public Transform[] patrolPoints;
     public Transform playerKillPos;
 
     // countdowns/timer
-    public float demonTransformCountdown; // time until toy transforms
-    public float demonFormCountdown;      // time until demon form ends
+    public float timeToTransform;
+    public float TransformedTimer = 10;
+    public float timeToRevert;
+    public float revertTimer = 30;
 
     public bool inToyForm;
     
-    // Use this for initialization
     void Start () {
-        NMA = GetComponent<NavMeshAgent>();
-        player = GameObject.Find("PLAYER1");
-        //playerCam = GameObject.Find("FPSController");
-        rb = GetComponent<Rigidbody>();
         isSearching = false;
-        demonTransformCountdown = 10;
-        demonFormCountdown = 10;
         inToyForm = true;
-
     }
 
-    // Update is called once per frame
     void Update() {
 
-        //=================================================================================
-        // Countdowns and timers
-        //=================================================================================
-        
-        // Countdown to demon form
-        if (demonTransformCountdown >= 0 && inToyForm) {
-            demonTransformCountdown -= Time.deltaTime;
-        }
-
-        // In demon form, countdown to toy form
-        if (demonFormCountdown >= 0 && !inToyForm) {
-            demonFormCountdown -= Time.deltaTime;
-        }
-
-        //=================================================================================
-        // Timers  while in toy/demon form
-        //=================================================================================
-        
-        // when in TOY form, and not 'taken care of' and countdown reaches 0, transform to demon
-        if (demonTransformCountdown <= 0 && inToyForm) {
-            DemonForm();
-        }
-
-        // when in DEMON form, and conditions met, turns back to toy form
-        if (demonFormCountdown <= 0 && !inToyForm) {
-            ToyForm();
-        }
-
-        // CONTINUOUSLY FOLLOW PLAYER
-        if (isSearching && !inToyForm) {
-            FollowPlayer();
-        }
-        else if (!isSearching && inToyForm) {
-            StopSearching();
-        }
-        
-        // TO SEARCH LAST KNOWN PLAYER POS OR 'DISTRACTION' , CALL FollowPlayer()
     }
 
-    // FUNCIONS 
-
-    public void DemonForm() {
-        demonFormCountdown = 10;
+    
+    /*
+    public virtual void DemonForm() {
         NMA.isStopped = false;
         inToyForm = false;
         isSearching = true;
-        rb.AddForce(0, 10, 0);
-        this.gameObject.transform.localScale = new Vector3(5, 5, 5);
-
+        RB.AddForce(0, 10, 0);
+        this.gameObject.transform.localScale = new Vector3(2.5f, 2.5f, 2.5f); //scale size
+        timeToRevert = revertTimer;
     }
 
-    public void ToyForm() {
+    public virtual void ToyForm() {
         StopSearching();
-        demonTransformCountdown = 10;
         inToyForm = true;
-        this.gameObject.transform.localScale = new Vector3(1, 1, 1);
+        this.gameObject.transform.localScale = new Vector3(1, 1, 1); // scale size
+        timeToTransform = TransformedTimer;
     }
 
-    public void FollowPlayer() {
+    public virtual void FollowPlayer() {
         NMA.destination = player.transform.position;
     }
 
-    public void StopSearching() {
-        rb.velocity = new Vector3(0, 0, 0);
+    public virtual void StopSearching() {
+        RB.velocity = new Vector3(0, 0, 0);
         NMA.isStopped = true;
         isSearching = false;
     }
@@ -128,16 +80,14 @@ public class NPC : MonoBehaviour {
         // DO PATROL STUFF
     }
 
-    private void ResetTimer() {
-        demonTransformCountdown = 10;
-    }
 
     private void OnTriggerEnter(Collider other) {
 
-        if (other.gameObject == player && isSearching) {
+        if (other.gameObject == player && !inToyForm) {
             StopSearching();
             KillPlayer();
         }
     }
+    */
 
 }
---
+++ End Game/Assets/Scripts/NPC/RedPanda.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class RedPanda : NPC {
+
+    private GameObject player;
+    private Rigidbody RB;
+    private NavMeshAgent NMA;
+
+    void Start() {
+        RB = GetComponent<Rigidbody>();
+        NMA = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("FPSController");
+
+        timeToTransform = TransformedTimer;
+        timeToRevert = revertTimer;
+        isSearching = false;
+        inToyForm = true;
+    }
+
+    void Update() {
+        //=================================================================================
+        // Countdowns and timers
+        //=================================================================================
+
+        // Countdown to demon form
+        if (timeToTransform >= 0 && inToyForm) {
+            timeToTransform -= Time.deltaTime;
+        }
+
+        // In demon form, countdown to toy form
+        if (timeToRevert >= 0 && !inToyForm) {
+            timeToRevert -= Time.deltaTime;
+        }
+
+        //=================================================================================
+        // Timers  while in toy/demon form
+        //=================================================================================
+
+        // when in TOY form, and not 'taken care of' and countdown reaches 0, transform to demon
+        if (timeToTransform <= 0 && inToyForm) {
+            DemonForm();
+        }
+
+        // when in DEMON form, and conditions met, turns back to toy form
+        if (timeToRevert <= 0 && !inToyForm) {
+            ToyForm();
+        }
+    }
+
+    public void FixedUpdate() {
+        if (isSearching && !inToyForm) {
+            FollowPlayer();
+        }
+        else if (!isSearching && inToyForm) {
+            StopSearching();
+        }
+    }
+
+    public void DemonForm() {
+        NMA.isStopped = false;
+        inToyForm = false;
+        isSearching = true;
+        RB.AddForce(0, 10, 0);
+        this.gameObject.transform.localScale = new Vector3(2.5f, 2.5f, 2.5f); //scale size
+        timeToRevert = revertTimer;
+    }
+
+    public void ToyForm() {
+        StopSearching();
+        inToyForm = true;
+        this.gameObject.transform.localScale = new Vector3(1, 1, 1); // scale size
+        timeToTransform = TransformedTimer;
+    }
+
+    public void FollowPlayer() {
+        NMA.destination = player.transform.position;
+    }
+
+    public void StopSearching() {
+        RB.velocity = new Vector3(0, 0, 0);
+        NMA.isStopped = true;
+        isSearching = false;
+    }
+
+    public void KillPlayer() {
+        // CALL CAMERA FUNTION FROM PLAYER SCRIPT
+        // PUT PLAYER INFRONT OF MONSTER
+        //playerCam.transform.position = playerKillPos.position;
+
+        // GET PLAYER TO FACE MONSTER
+        //I AM HERE
+
+        // PLAY KILL ANIMATION
+
+        // SET GAMEOVER THINGS
+    }
+
+    public void Patrol() {
+        // DO PATROL STUFF
+    }
+
+
+    private void OnTriggerEnter(Collider other) {
+
+        if (other.gameObject == player && !inToyForm) {
+            StopSearching();
+            KillPlayer();
+        }
+    }
+
+}
