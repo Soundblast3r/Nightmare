93f15d662d897ac7477fce7c7e5b4993ea38b422
AIE\s162871 <liamcantfly@gmail.com>
2018-08-01 12:34:10 +1000

    finished the hiding and climbing and started the jack in the box script

---
+++ End Game/Assets/Scripts/liam scripts/JackInTheBox.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JackInTheBox : MonoBehaviour
+{
+    GameObject jackinthebox;
+    float timer = 0;
+    public float timerIncrament = 100;
+    public List<GameObject> Jackboxs = new List<GameObject>();
+	// Use this for initialization
+	void Start ()
+    {
+        //jackinthebox = GameObject.FindGameObjectsWithTag("JacknBox");
+        //boxs.Add(GameObject.FindGameObjectsWithTag("JacknBox");
+        Debug.Log(Jackboxs.Count);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Time.time > timer)
+        {
+            //do thing
+            //set the timer to the current time plus the amount
+            //of time you want to wait in seconds 
+            timer = Time.time + timerIncrament;
+        }
+	}
+
+
+    void RandomSpawn()
+    {
+        //stores the Random number in an int 
+        //used to spawn The JACK IN THE BOX
+        int JackCount;
+        JackCount = Random.Range(0, Jackboxs.Count -1);
+
+        //checks to see if it's not null
+        if(Jackboxs.Count > 0)
+        {
+            //spawns the jack in the box 
+            //wherever the number compeared to
+            Jackboxs[JackCount].SetActive(true);
+        }
+    }
+}
