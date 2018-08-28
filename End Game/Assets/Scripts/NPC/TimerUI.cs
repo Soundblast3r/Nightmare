78204accdffc6e808ddcf7a21d0fd210096ae38d
AIE\s162868 <edwardngo92@gmail.com>
2018-08-28 16:02:43 +1000

    Added GUI template, timer UI script

---
+++ End Game/Assets/Scripts/NPC/TimerUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//=======================================================
+// Created by: Edward Ngo
+// Contributors:
+//=======================================================
+
+public class TimerUI : MonoBehaviour {
+
+    private Crocodile Crocodile;
+    //private GameObject Bear;
+    //private GameObject Panda;   
+    //private GameObject Owl;
+
+    public Text CrocTimer, BearTimer, PandaTimer, OwlTimer;
+
+    // Use this for initialization
+    void Start () {
+        Crocodile = GameObject.FindGameObjectWithTag("Crocodile").GetComponent<Crocodile>();
+        //Bear = GameObject.FindGameObjectWithTag("Bear").GetComponen;
+        //Panda = GameObject.FindGameObjectWithTag("RedPanda");
+        //Owl =  GameObject.FindGameObjectWithTag("Owl");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        CrocTimer.text = Crocodile.timeToTransform.ToString("F0");
+    }
+}
