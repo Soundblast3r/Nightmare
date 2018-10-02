352afde5373d84edcd09d1b1b5f50ea1d064af23
Cappy <stirrupprazak.c@gmail.com>
2018-10-03 09:57:58 +1000

    Updated Menu Scene Lighting, Appearance
    Updated Menu Layout
    
    Rewrote Menu Script.

--- End Game/Assets/Scripts/Menu.cs
+++ End Game/Assets/Scripts/Menu.cs
@@ -7,113 +7,45 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    //Make sure to attach these Buttons in the Inspector
-    public GameObject NewGameButton;
-    public GameObject ContinueButton;
-    public GameObject QuitGameButton;
-    public GameObject OptionsButton;
-  //  public GameObject UnlockablesButton;
-    public bool UnlockablesUnlocked;
-    //public Button OptionsBackButton;
-    //public Button UnlockablesBackButton;
 
     //Panels
-    public GameObject MainMenuPanel;
-    public GameObject OptionsPanel;
-  //  public GameObject UnlockablesPanel;
+    public GameObject mainMenuPanel;
+    public GameObject controlsPanel;
+
+    //Bools
+    private bool controlsActive = false;
+    
     void Start()
     {
-        MainMenuPanel.SetActive(true);
-        OptionsPanel.SetActive(false);
-   //     UnlockablesPanel.SetActive(false);
-
-        {
-            
-        }
+        mainMenuPanel.SetActive(true);
+        controlsPanel.SetActive(false);
     }
 
-    public void StartNew()
+    public void StartGame()
     {
-        //Debug.Log("New Game Initiated");
         SceneManager.LoadScene(1);
     }
 
-    ///LoadGame
-    public void StartLoad()
-    {
-        //Output this to console when the Button is clicked
-        //Debug.Log("Loading Game");
-        //Debug.Log("Load Game Unavailable");
-    }
-
     ///Quit
-    public void Terminate()
+    public void ExitGame()
     {
-        //Output this to console when the Button is clicked
-        //Debug.Log("Game Terminated");
-        //EditorApplication.isPlaying = false;
         Application.Quit();
     }
 
-    ///Options
-    public void ChangeOptions()
+    ///Toggles the Controls Panel
+    public void ControlsToggle()
     {
-        //Output this to console when the Button is clicked
-        //Debug.Log("Options Opened");
-
-        MainMenuPanel.SetActive(false);
-        OptionsPanel.SetActive(true);
-   //     UnlockablesPanel.SetActive(false);
-    }
-
-    ///Unlockables
-    public void Extras()
-    {
-        //Output this to console when the Button is clicked
-     //   if (UnlockablesButton.activeSelf == true)
+        if (controlsActive)
         {
-      //      //Debug.Log("Unlockables opened");
-      //
-      //      MainMenuPanel.SetActive(false);
-      //      OptionsPanel.SetActive(false);
-     //       UnlockablesPanel.SetActive(true);
+            mainMenuPanel.SetActive(false);
+            controlsPanel.SetActive(true);
+            controlsActive = true;
         }
-    }
-
-    public void CloseOptions()
-    {
-        //Output this to console when the Button is clicked
-        //Debug.Log("Options Closed");
-
-        OptionsPanel.SetActive(false);
-     //   UnlockablesPanel.SetActive(false);
-        MainMenuPanel.SetActive(true);
-    }
-
-    ///Unlockables
-    public void CloseExtras()
-    {
-        //Output this to console when the Button is clicked
-      // if (UnlockablesPanel.activeSelf == true)
-      // {
-      //     //Debug.Log("Unlockables Closed");
-      //
-      //     OptionsPanel.SetActive(false);
-      //     UnlockablesPanel.SetActive(false);
-      //     MainMenuPanel.SetActive(true);
-      // }
-    }
-
-    private void Update()
-    {
-        //     if (UnlockablesUnlocked == true)
-        //     {
-        //         UnlockablesButton.SetActive(true);
-        //     }
-        //
-        //     else
-        //     {
-        //         UnlockablesButton.SetActive(false);
-        //     }
+        else
+        {
+            mainMenuPanel.SetActive(true);
+            controlsPanel.SetActive(false);
+            controlsActive = false;
+        }
     }
 }
