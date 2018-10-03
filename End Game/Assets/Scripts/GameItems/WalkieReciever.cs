b412739e5c6f2cd97f229ca9925edf0ecb21b6cc
AIE\s162868 <edwardngo92@gmail.com>
2018-10-03 12:14:39 +1000

    added walky reciever prefab, now displays channel

--- End Game/Assets/Scripts/GameItems/WalkieReciever.cs
+++ End Game/Assets/Scripts/GameItems/WalkieReciever.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(BoxCollider))]
 
 public class WalkieReciever : MonoBehaviour {
+
+    private GameObject playerCam;
+    private GameObject displayWalkyNumber;
+    private bool displayNumberSet = false;
 
     public int walkyNumber;
 
@@ -13,6 +18,8 @@ public class WalkieReciever : MonoBehaviour {
     public float soundDurationMax;
 
     void Start () {
+        playerCam = GameObject.Find("FirstPersonCharacter");
+        displayWalkyNumber = transform.Find("Canvas/displayChannelNumber").gameObject;
         GetComponent<BoxCollider>().isTrigger = true;
         walkyNumber = 0;
         emittingSound = false;
@@ -20,6 +27,15 @@ public class WalkieReciever : MonoBehaviour {
 	}
 	
 	void Update () {
+
+        if (!displayNumberSet) {
+            displayWalkyNumber.GetComponent<Text>().text = walkyNumber.ToString();
+            displayNumberSet = true;
+        }
+
+        if (displayNumberSet) { // rotates on all axis instead of only Y, need fix?
+            displayWalkyNumber.transform.LookAt(playerCam.transform.position);
+        }
 
         if (emittingSound && soundDuration >= 0) {
             soundDuration -= Time.deltaTime;
