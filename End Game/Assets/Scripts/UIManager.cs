cab31bb92d1918de0b60dda61899f3d2c75d9e53
Cappy <stirrupprazak.c@gmail.com>
2018-10-15 15:51:57 +1100

    created Beta Game Scene (NEEDS MECHANICS SETUP)

---
+++ End Game/Assets/Scripts/UIManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    //This is a temporary purpose to introduce button functionality
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
